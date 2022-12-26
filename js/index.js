1229a2a993dc361154b1e514c65ae382be3b3e0f
Santi <84866698+heisenbergg-0@users.noreply.github.com>
2022-12-26 18:14:06 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('Sea el camino que elija, todos me llevarán siempre a ti');
+    alert('Te amo mucho mi flaca<3');
+    location.href = 'https://www.youtube.com/watch?v=UojBaKX5Vz4&ab_channel=ErosRamazzottiVEVO';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('Nombre y Apellido');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
